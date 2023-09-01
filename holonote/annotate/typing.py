e58f26a10649621d9a6aa68657aab975be78030e
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-01 11:27:00 +0200

    Add some docstring

--- holonote/annotate/typing.py
+++ holonote/annotate/typing.py
@@ -4,6 +4,13 @@ from typing import Callable, Literal, TypedDict
 
 
 class SpecItem(TypedDict):
+    """Contains the type and region specification for a region.
+
+    The type is a callable that can be used to convert the data to the
+    correct type. This could be `np.datetime64` or `float` for example.
+
+    The region specification is either "range", "single", or "multi".
+    """
     type: Callable
     region: Literal["range"] | Literal["single"] | Literal["multi"]
 
