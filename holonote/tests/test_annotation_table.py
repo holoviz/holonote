3ec086143aa38abe0d3e4a179e9596d88f4914ba
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-28 17:12:08 +0200

    Update table tests

--- holonote/tests/test_annotation_table.py
+++ holonote/tests/test_annotation_table.py
@@ -1,20 +1,34 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import numpy as np
 import pandas as pd
+import pytest
 
 from holonote.annotate import AnnotationTable
 
+if TYPE_CHECKING:
+    from holonote.annotate.typing import SpecDict
 
-def test_table_single_kdim() -> None:
-    spec = {"TIME": {"type": np.datetime64, "region": "range"}}
+
+def _init_table(spec: SpecDict) -> AnnotationTable:
     table = AnnotationTable()
     table.load(primary_key_name="id", fields=["test_description"])
     assert len(table._region_df) == 0, "Should be initialized empty"
     assert tuple(table._region_df.columns) == AnnotationTable.columns
+    return table
 
-    start = pd.Timestamp("2022-06-17")
-    end = pd.Timestamp("2022-06-19")
+
+def _time_range():
+    return (pd.Timestamp("2022-06-17"), pd.Timestamp("2022-06-19"))
+
+
+def test_table_single_kdim() -> None:
+    spec = {"TIME": {"type": np.datetime64, "region": "range"}}
+    table = _init_table(spec)
+
+    start, end = _time_range()
     regions = {"TIME": (start, end)}
     table.add_annotation(regions, spec=spec, id=100, test_description="A test")
 
@@ -37,19 +51,17 @@ def test_table_multiple_kdim() -> None:
         "TIME": {"type": np.datetime64, "region": "range"},
         "x": {"type": float, "region": "point"},
     }
-    table = AnnotationTable()
-    table.load(primary_key_name="id", fields=["test_description"])
+    table = _init_table(spec)
 
-    start = pd.Timestamp("2022-06-17")
-    end = pd.Timestamp("2022-06-19")
+    start, end = _time_range()
     regions = {"x": 1, "TIME": (start, end)}
     table.add_annotation(regions, spec=spec, id=100, test_description="A test")
 
     d = {
-        "region": {0: "point", 1: "range"},
-        "dim": {0: "x", 1: "TIME"},
-        "value": {0: 1, 1: (start, end)},
-        "_id": {0: 100, 1: 100},
+        "region": ["point", "range"],
+        "dim": ["x", "TIME"],
+        "value": [1, (start, end)],
+        "_id": [100, 100],
     }
     expected = pd.DataFrame(d).astype({"_id": object})
     pd.testing.assert_frame_equal(table._region_df, expected)
@@ -57,7 +69,7 @@ def test_table_multiple_kdim() -> None:
     d = {
         "start[TIME]": {100: start},
         "end[TIME]": {100: end},
-        "point[x]": {100: 1.},
+        "point[x]": {100: 1.0},
         "test_description": {100: "A test"},
     }
     expected = pd.DataFrame(d)
@@ -70,21 +82,19 @@ def test_table_multiple_kdim_and_annotations() -> None:
         "TIME": {"type": np.datetime64, "region": "range"},
         "x": {"type": float, "region": "point"},
     }
-    table = AnnotationTable()
-    table.load(primary_key_name="id", fields=["test_description"])
+    table = _init_table(spec)
 
-    start = pd.Timestamp("2022-06-17")
-    end = pd.Timestamp("2022-06-19")
+    start, end = _time_range()
     regions = {"x": 1, "TIME": (start, end)}
     table.add_annotation(regions, spec=spec, id=100, test_description="101")
     regions = {"x": 2}
     table.add_annotation(regions, spec=spec, id=101, test_description="102")
 
     d = {
-        "region": {0: "point", 1: "range", 2: "point"},
-        "dim": {0: "x", 1: "TIME", 2: "x"},
-        "value": {0: 1, 1: (start, end), 2: 2},
-        "_id": {0: 100, 1: 100, 2: 101},
+        "region": ["point", "range", "point"],
+        "dim": ["x", "TIME", "x"],
+        "value": [1, (start, end), 2],
+        "_id": [100, 100, 101],
     }
     expected = pd.DataFrame(d).astype({"_id": object})
     pd.testing.assert_frame_equal(table._region_df, expected)
@@ -92,7 +102,7 @@ def test_table_multiple_kdim_and_annotations() -> None:
     d = {
         "start[TIME]": {100: start, 101: pd.NaT},
         "end[TIME]": {100: end, 101: pd.NaT},
-        "point[x]": {100: 1., 101: 2.},
+        "point[x]": {100: 1.0, 101: 2.0},
         "test_description": {100: "101", 101: "102"},
     }
     expected = pd.DataFrame(d)
@@ -105,8 +115,7 @@ def test_only_adding_one_dim_with_multiple_dimensions() -> None:
         "TIME": {"type": np.datetime64, "region": "range"},
         "x": {"type": float, "region": "point"},
     }
-    table = AnnotationTable()
-    table.load(primary_key_name="id", fields=["test_description"])
+    table = _init_table(spec)
 
     regions = {"x": 1}
     table.add_annotation(regions, spec=spec, id=100, test_description="101")
@@ -114,10 +123,10 @@ def test_only_adding_one_dim_with_multiple_dimensions() -> None:
     table.add_annotation(regions, spec=spec, id=101, test_description="102")
 
     d = {
-        "region": {0: "point",  1: "point"},
-        "dim": {0: "x",  1: "x"},
-        "value": {0: 1., 1: 2.},
-        "_id": {0: 100,  1: 101},
+        "region": ["point", "point"],
+        "dim": ["x", "x"],
+        "value": [1, 2],
+        "_id": [100, 101],
     }
     expected = pd.DataFrame(d).astype({"_id": object, "value": object})
     pd.testing.assert_frame_equal(table._region_df, expected)
@@ -125,7 +134,7 @@ def test_only_adding_one_dim_with_multiple_dimensions() -> None:
     d = {
         "start[TIME]": {100: pd.NaT, 101: pd.NaT},
         "end[TIME]": {100: pd.NaT, 101: pd.NaT},
-        "point[x]": {100: 1., 101: 2.},
+        "point[x]": {100: 1.0, 101: 2.0},
         "test_description": {100: "101", 101: "102"},
     }
     expected = pd.DataFrame(d)
@@ -138,8 +147,7 @@ def test_nodata_multiple_dimension() -> None:
         "TIME": {"type": np.datetime64, "region": "range"},
         "x": {"type": float, "region": "point"},
     }
-    table = AnnotationTable()
-    table.load(primary_key_name="id", fields=["test_description"])
+    table = _init_table(spec)
 
     assert table._region_df.empty
     assert tuple(table._region_df.columns) == AnnotationTable.columns
@@ -153,6 +161,67 @@ def test_nodata_multiple_dimension() -> None:
     expected = pd.DataFrame(d).drop(index=0)
     expected.index.name = "id"
     output = table.get_dataframe(spec=spec)
+
+    assert output.empty
+    pd.testing.assert_series_equal(output.dtypes, expected.dtypes)
+    pd.testing.assert_index_equal(output.columns, expected.columns)
+
+
+@pytest.mark.parametrize("dim", ["x", "TIME"])
+def test_table_multiple_kdim_and_annotations_with_selected_dims(dim) -> None:
+    spec = {
+        "TIME": {"type": np.datetime64, "region": "range"},
+        "x": {"type": float, "region": "point"},
+    }
+    table = _init_table(spec)
+
+    start, end = _time_range()
+    regions = {"x": 1, "TIME": (start, end)}
+    table.add_annotation(regions, spec=spec, id=100, test_description="101")
+    regions = {"x": 2}
+    table.add_annotation(regions, spec=spec, id=101, test_description="102")
+
+    d = {
+        "region": ["point", "range", "point"],
+        "dim": ["x", "TIME", "x"],
+        "value": [1, (start, end), 2],
+        "_id": [100, 100, 101],
+    }
+    expected = pd.DataFrame(d).astype({"_id": object})
+    pd.testing.assert_frame_equal(table._region_df, expected)
+
+    d = {
+        "start[TIME]": {100: start, 101: pd.NaT},
+        "end[TIME]": {100: end, 101: pd.NaT},
+        "point[x]": {100: 1.0, 101: 2.0},
+        "test_description": {100: "101", 101: "102"},
+    }
+    d = {k: v for k, v in d.items() if "[" not in k or dim in k}
+    expected = pd.DataFrame(d)
+    expected.index.name = "id"
+    pd.testing.assert_frame_equal(table.get_dataframe(spec=spec, dims=[dim]), expected)
+
+
+@pytest.mark.parametrize("dim", ["x", "TIME"])
+def test_nodata_multiple_dimension_with_selected_dims(dim) -> None:
+    spec = {
+        "TIME": {"type": np.datetime64, "region": "range"},
+        "x": {"type": float, "region": "point"},
+    }
+    table = _init_table(spec)
+
+    assert table._region_df.empty
+
+    d = {
+        "start[TIME]": [pd.NaT],
+        "end[TIME]": [pd.NaT],
+        "point[x]": [0.0],
+        "test_description": [""],
+    }
+    d = {k: v for k, v in d.items() if "[" not in k or dim in k}
+    expected = pd.DataFrame(d).drop(index=0)
+    expected.index.name = "id"
+    output = table.get_dataframe(spec=spec, dims=[dim])
 
     assert output.empty
     pd.testing.assert_series_equal(output.dtypes, expected.dtypes)
