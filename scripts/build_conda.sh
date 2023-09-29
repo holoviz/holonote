c95f10a9eb429203090c3e8039705e6fa9932610
Simon Høxbro Hansen <simon.hansen@me.com>
2023-09-29 17:45:49 +0200

    fixes...

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eux pipefail
+set -euxo pipefail
 
 git status
+
+hatch build --clean
 
 export VERSION="$(echo "$(ls dist/*.whl)" | cut -d- -f2)"
 conda build conda/recipe --no-test --no-anaconda-upload --no-verify
