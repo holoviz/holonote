81d04d608462f0864d45f2c644c8fd9e93223aa6
Simon Høxbro Hansen <simon.hansen@me.com>
2023-10-06 11:42:40 +0200

    Add import test

--- conda/build.sh
+++ conda/build.sh
@@ -7,4 +7,4 @@ git status
 hatch build --clean
 
 export VERSION="$(hatch version)"
-conda build conda/recipe --no-test --no-anaconda-upload --no-verify
+conda build conda/recipe --no-anaconda-upload --no-verify
