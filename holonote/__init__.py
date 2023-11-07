1e297bc62ba378e30ac64d4dca61a09887b95fd3
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-07 17:49:45 +0100

    Don't use setuptools_scm if .git does not exists (#52)

--- holonote/__init__.py
+++ holonote/__init__.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 
+import os
+
 from . import annotate, editor  # noqa: F401
 
 # Define '__version__'
 try:
-    # If setuptools_scm is installed (e.g. in a development environment with
-    # an editable install), then use it to determine the version dynamically.
-    from setuptools_scm import get_version
+    if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):
+        # If setuptools_scm is installed (e.g. in a development environment with
+        # an editable install), then use it to determine the version dynamically.
+        from setuptools_scm import get_version
 
-    # This will fail with LookupError if the package is not installed in
-    # editable mode or if Git is not installed.
-    __version__ = get_version(root="..", relative_to=__file__)
-except (ImportError, LookupError):
+        # This will fail with LookupError if the package is not installed in
+        # editable mode or if Git is not installed.
+        __version__ = get_version(root="..", relative_to=__file__)
+    else:
+        raise FileNotFoundError
+except (ImportError, LookupError, FileNotFoundError):
     # As a fallback, use the version that is hard-coded in the file.
     try:
         from ._version import __version__
