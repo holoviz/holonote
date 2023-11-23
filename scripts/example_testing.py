a54aad1cb1bb5a733592fa0643be58a35df0b5e7
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-23 09:30:02 +0100

    Handle nested folder better

--- scripts/example_testing.py
+++ scripts/example_testing.py
@@ -15,11 +15,13 @@ def main() -> None:
         tmpdir = Path(tmpdir)
         shutil.copy(example_path / ".." / "pyproject.toml", tmpdir)
 
-        for file in example_path.rglob("*.ipynb"):
-            test_folder = tmpdir / uuid4().hex[:8] / file.relative_to(example_path).parent
-            test_folder.mkdir(parents=True, exist_ok=True)
-            example_file = test_folder / file.name
-            shutil.copy(file, example_file)
+        for file in sorted(example_path.rglob("*.ipynb")):
+            if file.parent.name == ".ipynb_checkpoints":
+                continue
+            test_folder = tmpdir / uuid4().hex[:8]
+            test_file = test_folder / file.relative_to(example_path)
+            test_file.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy(file, test_file)
             shutil.copytree(example_path / "assets", test_folder / "assets")
 
         os.chdir(tmpdir)
