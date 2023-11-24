f8cf2c1ccfef4e4d9b97d47dcb76d9655e5449ad
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-24 10:20:54 +0100

    Try using pytest-codspeed

--- holonote/tests/conftest.py
+++ holonote/tests/conftest.py
@@ -10,6 +10,40 @@ import pytest
 
 from holonote.annotate import Annotator, SQLiteDB, UUIDHexStringKey
 
+optional_markers = {
+    "benchmark": {
+        "help": "Run benchmarks",
+        "marker-descr": "Benchmark test marker",
+        "skip-reason": "Test only runs with the --benchmark option.",
+    },
+}
+
+
+def pytest_addoption(parser):
+    for marker, info in optional_markers.items():
+        parser.addoption(f"--{marker}", action="store_true", default=False, help=info["help"])
+
+
+def pytest_configure(config):
+    for marker, info in optional_markers.items():
+        config.addinivalue_line("markers", "{}: {}".format(marker, info["marker-descr"]))
+
+
+def pytest_collection_modifyitems(config, items):
+    skipped, selected = [], []
+    markers = [m for m in optional_markers if config.getoption(f"--{m}")]
+    empty = not markers
+    for item in items:
+        if empty and any(m in item.keywords for m in optional_markers):
+            skipped.append(item)
+        elif empty or (not empty and any(m in item.keywords for m in markers)):
+            selected.append(item)
+        else:
+            skipped.append(item)
+
+    config.hook.pytest_deselected(items=skipped)
+    items[:] = selected
+
 
 @pytest.fixture()
 def conn_sqlite_uuid(tmp_path) -> Iterator[SQLiteDB]:
