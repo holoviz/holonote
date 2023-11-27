5f7812542d89a2e884ecc692b86c76371e366da9
Simon Høxbro Hansen <simon.hansen@me.com>
2023-11-27 10:05:09 +0100

    Add commit benchmark test

--- holonote/tests/benchmark/test_benchmarks.py
+++ holonote/tests/benchmark/test_benchmarks.py
@@ -7,8 +7,7 @@ from holonote.annotate import Annotator, SQLiteDB
 pytestmark = pytest.mark.benchmark
 
 
-@pytest.mark.parametrize("items", [10, 100, 1_000])
-def test_define_annotations(benchmark, items):
+def setup_annotator(items) -> tuple[Annotator, pd.DataFrame]:
     rng = np.random.default_rng(1337)
     start_time = np.arange(0, items * 2, 2)
     end_time = start_time + 1
@@ -21,7 +20,23 @@ def test_define_annotations(benchmark, items):
     )
     annotator = Annotator({"TIME": int}, connector=SQLiteDB(filename=":memory:"))
     annotator.define_annotations(data, TIME=("start_time", "end_time"))
+    return annotator, data
+
+
+@pytest.mark.parametrize("items", [10, 100, 1000])
+def test_define_annotations(benchmark, items) -> None:
+    annotator, data = setup_annotator(items)
 
     @benchmark
-    def bench():
+    def bench() -> None:
         annotator.define_annotations(data, TIME=("start_time", "end_time"))
+
+
+@pytest.mark.parametrize("items", [10, 100, 1000])
+def test_commit(benchmark, items) -> None:
+    annotator, data = setup_annotator(items)
+    annotator.define_annotations(data, TIME=("start_time", "end_time"))
+
+    @benchmark
+    def bench() -> None:
+        annotator.commit()
