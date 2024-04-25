de0a6f35fcfdc51e38db18e62711a26ddc6a2b1b
Andrew Huang <ahuang@anaconda.com>
2024-04-25 10:59:06 -0700

    add test

--- holonote/tests/test_app.py
+++ holonote/tests/test_app.py
@@ -9,3 +9,12 @@ def test_panel_app(annotator_range1d):
     w = PanelWidgets(annotator_range1d)
     assert isinstance(w.fields_widgets, pn.Column)
     assert isinstance(w.tool_widgets, pn.Row)
+
+
+def test_as_popup(annotator_range1d):
+    w = PanelWidgets(annotator_range1d, as_popup=True)
+    assert not w._layout.visible
+    for display in w.annotator._displays.values():
+        assert display._edit_streams[0].popup
+        assert display._tap_stream.popup
+    assert w.__panel__().visible
