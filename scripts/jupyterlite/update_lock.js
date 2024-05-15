a49c6896730c294db3c4c1a618170e07beeeae61
Simon Høxbro Hansen <simon.hansen@me.com>
2024-05-15 12:49:06 +0200

    Update jupyterlite files (#107)

--- scripts/jupyterlite/update_lock.js
+++ scripts/jupyterlite/update_lock.js
@@ -1,23 +1,20 @@
-const { loadPyodide } = require("pyodide")
-const fs = require("fs")
+const { loadPyodide } = require("pyodide");
+const fs = require("fs");
 
 async function main() {
-  let pyodide = await loadPyodide()
-  await pyodide.loadPackage(["micropip"])
+  const extra = fs.readFileSync("extra_packages.json", "utf8");
+
+  let pyodide = await loadPyodide();
+  await pyodide.loadPackage(["micropip"]);
 
   output = await pyodide.runPythonAsync(`
-    import micropip
-
-    await micropip.install([
-      "https://cdn.holoviz.org/panel/1.3.5/dist/wheels/bokeh-3.3.2-py3-none-any.whl",
-      "https://cdn.holoviz.org/panel/1.3.5/dist/wheels/panel-1.3.5-py3-none-any.whl",
-      "https://files.pythonhosted.org/packages/7f/0e/f3e09ad030185c5f2bb03265778699c8f80d7481b1e0abbb2a12c63fe093/holoviews-1.18.2a2-py2.py3-none-any.whl",
-      "holonote",
-      "hvplot",
-    ])
-
-    micropip.freeze()
-    `)
-  fs.writeFileSync("pyodide-lock.json", output)
+import json
+import micropip
+extra = json.loads("""${extra}""")
+await micropip.install(extra)
+micropip.freeze()
+`);
+  fs.writeFileSync("pyodide-lock.json", output);
 }
-main()
+
+main();
