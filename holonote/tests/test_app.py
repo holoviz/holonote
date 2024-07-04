5ff0b8a621b3894156a4d6320ac446f27ace5fe8
Simon Høxbro Hansen <simon.hansen@me.com>
2024-07-04 14:44:05 +0200

    Add tabulator test

--- holonote/tests/test_app.py
+++ holonote/tests/test_app.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import numpy as np
+import pandas as pd
 import panel as pn
 
-from holonote.app import PanelWidgets
+from holonote.app import AnnotatorTabulator, PanelWidgets
 
 
 def test_panel_app(annotator_range1d):
@@ -18,3 +20,19 @@ def test_as_popup(annotator_range1d):
         assert display._edit_streams[0].popup
         assert display._tap_stream.popup
     assert w.__panel__().visible
+
+
+def test_tabulator(annotator_range1d):
+    t = AnnotatorTabulator(annotator_range1d)
+    assert isinstance(t.tabulator, pn.widgets.Tabulator)
+
+    annotator_range1d.set_regions(TIME=(np.datetime64("2022-06-06"), np.datetime64("2022-06-08")))
+    annotator_range1d.add_annotation(description="A test annotation!")
+    assert len(t.tabulator.value) == 1
+    assert t.tabulator.value.iloc[0, 0] == pd.Timestamp("2022-06-06")
+    assert t.tabulator.value.iloc[0, 1] == pd.Timestamp("2022-06-08")
+    assert t.tabulator.value.iloc[0, 2] == "A test annotation!"
+    assert "darkgray" in t.tabulator.style.to_html()
+
+    annotator_range1d.commit(return_commits=True)
+    assert "darkgray" not in t.tabulator.style.to_html()
